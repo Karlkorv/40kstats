f465fc223c1e62f340244465af26d4dc88d5f254
Janne Schyffert <jannesc@kth.se>
2024-03-14 11:18:43 +0100

    Add basic firestore integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 
 export function App({model : LeaderboardModel}) {
-    function addCallbackACB(event) {
-        addToFirestore(event.target.value);
-    }
     return (
         <div>
-            <input>
-                <button onClick={addCallbackACB}>Add to Firestore</button>
-            </input>
+            Hello there
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
+import {initializeModel} from "./model/LeaderboardModel.ts";
 import React from "react";
-createRoot(document.getElementById("root")!).render(<App />); // root element always 100% exists, no need to null check
+
+const model = initializeModel();
+
+createRoot(document.getElementById("root")!).render(<App model={model} />); // root element always 100% exists, no need to null check
