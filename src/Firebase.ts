e415e7f2e9946b74a7c10bcd3d615dffd7ac0b93
Janne Schyffert <jannesc@kth.se>
2024-03-14 11:11:45 +0100

    Update leaderboardmode, match to use date as ID

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,7 +1,7 @@
 import { firebaseConfig } from './firebaseConfig';
 import { initializeApp } from 'firebase/app';
 import {Match} from "./model/Match.ts";
-import {addDoc, collection, getDoc, getFirestore, doc} from 'firebase/firestore'; 
+import { addDoc, setDoc, collection, getDoc, getFirestore, doc, query, orderBy, limit, getDocs} from 'firebase/firestore'; 
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
@@ -9,13 +9,12 @@ const db = getFirestore(app);
 const matchRef = collection(db, "matches");
 
 export function addMatch(match: Match) {
-    addDoc(collection(db, "matches"), match);
-}
-
-export function initializeModel() {
-    
+    // Potential bug here if the match already exists
+    setDoc(doc(matchRef, match.date.toString()), match).then(() => {
+        console.log("Match added:" + match.date.toString());
+    });
 }
 
 export function getMatches(amount: number) {
-
+    return getDocs(query(matchRef, orderBy("date", "desc"), limit(amount)))
 }
