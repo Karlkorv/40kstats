4e00ff6b3f8db2d60f6845119a3609873c051f38
Janne Schyffert <jannesc@kth.se>
2024-04-10 15:15:13 +0200

    Remove dummy match duplicate button

--- src/presenters/homePresenter.tsx
+++ src/presenters/homePresenter.tsx
@@ -1,28 +1,16 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { MatchCreatorView } from "../views/matchCreatorView.tsx";
-import { Match } from "../model/match.ts";
-import { FACTIONS } from "../model/factions.ts";
+import { LeaderBoardModel } from "../model/LeaderboardModel";
 
 const HomeScreen = observer(
-	function HomePresenter({ model }: any) {
+	function HomePresenter({ model }: { model: LeaderBoardModel }) {
 		function createMatchClick(evt) {
 			model.startMatchCreation();
 			window.location.hash = "#/matchCreator"
 		}
 
-		function addDummyMatch() {
-			model.addMatch(new Match(
-				["Janne", "Johan"],
-				[FACTIONS.ADEPTUS_MECHANICUS, FACTIONS.AELDARI],
-				["Janne"],
-				[10, 0], [0, 0])
-			);
-		}
-
 		return (
 			<div>
-				<button onClick={addDummyMatch}>Add dummy match</button>
 				<button onClick={createMatchClick}>Create a match</button>
 			</div>
 		);
