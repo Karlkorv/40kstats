d08235a6d09b55d13f9cd7e84740a06a77764219
Desktop <ptybdjgamer@hotmail.com>
2024-04-19 16:21:40 +0200

    Merge main into branch

--- src/views/navbarView.tsx
+++ src/views/navbarView.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { IconButton, Button, AppBar } from "@mui/material";
+import { IconButton, Button, AppBar, Toolbar } from "@mui/material";
 import { Home } from "@mui/icons-material";
 
 export function NavbarView({model, handleHomeButtonClick, handleCreateMatchButtonClick, handleLoginButtonClick, handleLogoutButtonClick}) {
@@ -22,27 +22,29 @@ export function NavbarView({model, handleHomeButtonClick, handleCreateMatchButto
     return (
         <div className="navbar">
             <AppBar>
-                <div className="home-button">
-                    <IconButton onClick={(e) => onHomeButtonClickACB(e)}>
-                        <Home/>
-                    </IconButton>
-                </div>
-                <div className="create-match-button">
-                    <Button variant="contained" disabled={!model.user} onClick={(e) => onCreateMatchButtonClick(e)}>
-                        {model.user && "Create Match" || !model.user && "Sign in to create match"}
-                    </Button>
-                </div>
-                {model.user && <div className="login-button">
-                    <Button variant="contained" onClick={(e) => onLoginButtonClick(e)}>
-                        Login
-                    </Button>
-                </div>}
+                <Toolbar>
+                    <div className="home-button">
+                        <IconButton onClick={(e) => onHomeButtonClickACB(e)}>
+                            <Home/>
+                        </IconButton>
+                    </div>
+                    <div className="create-match-button">
+                        <Button variant="contained" disabled={!model.user} onClick={(e) => onCreateMatchButtonClick(e)}>
+                            {model.user && "Create Match" || !model.user && "Sign in to create match"}
+                        </Button>
+                    </div>
+                    {!model.user && <div className="login-button">
+                        <Button variant="contained" onClick={(e) => onLoginButtonClick(e)}>
+                            Login
+                        </Button>
+                    </div>}
 
-                {!model.user && <div className="logout-button">
-                    <Button variant="contained" onClick={(e) => onLogoutButtonClick(e)}>
-                        Logout
-                    </Button>
-                </div>}
+                    {model.user && <div className="logout-button">
+                        <Button variant="contained" onClick={(e) => onLogoutButtonClick(e)}>
+                            Logout
+                        </Button>
+                    </div>}
+                </Toolbar>
             </AppBar>
         </div>
     );
