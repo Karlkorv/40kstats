c00689a88c79c4dc590a611cd7982ab0f0618704
Janne Schyffert <jannesc@kth.se>
2024-05-23 11:13:57 +0200

    fix: Firestore database url missing

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -10,6 +10,7 @@ import { DEFAULT_CREATE_MATCH, MatchCreatorInput } from './model/FormModel.ts';
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
     authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
     projectId: import.meta.env.VITE_PROJECT_ID,
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
